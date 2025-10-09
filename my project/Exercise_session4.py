96c8de37e27c898d083327d3ab3437b0e3d67af9
MDAS134971807 <haddadiaval49@gmail.com>
2025-10-09 08:50:01 +0330

    Add files via upload

---
+++ my project/Exercise_session4.py
@@ -0,0 +1,11 @@
+midterm = float(input("Enter your midterm score:"))
+final = float(input("Enter your final score:"))
+average = (midterm + final)/2
+if (0 <= midterm <= 20) and (0 <= final <=20) and (average >= 10):
+    print("Average:", average , "=>passed")
+elif (0 <= midterm <= 20) and (0 <= final <=20) and (average <= 10): 
+    print("Average:", average , "=>failed")
+else:
+    print("Error:", "scores are not defined")
+
+
